1202b6e9140be6f66dc3b7f8f27c302b8fec4682
WilliamsChazal <wchazal@codeur.online>
2021-06-16 18:00:30 +0200

    include-navbar

--- test.php
+++ test.php
@@ -5,22 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet" rel="stylesheet">
     <link rel="stylesheet" href="test.css">
     <title>I Will Design</title>
 </head>
 <body>
-    <section class='main'>
-        <nav>
+<?php include ('includes/navbar/navbar.php')?>
+<!-- <nav>
             <div>Logo</div>
             <ul>
                 <li>Qui suis je </li>
                 <li>Que sais je faire</li>
                 <li>Qu'ai je fait</li>
                 <li>contact</li>
+                <button>Dark mode</button>
             </ul>
-        </nav>
-
+        </nav> -->
+    <section class='main'>
             <div class='main_text'>
                 <div class='main_text--aera'>
                     <h1>Williams Chazal</h1>
