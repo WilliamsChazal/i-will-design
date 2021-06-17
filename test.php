986ad9af2a548f31f502fa0e81bffbf5e5d4fc46
WilliamsChazal <wchazal@codeur.online>
2021-06-17 18:19:09 +0200

    new commit

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
                 <div class='main_text--aera'>
                      <h1 class='bounce-in-left'>Williams Chazal</h1>
                     <div class='bounce-in-left'> <h3 class="main_text--slogan">"Des idées au pluriel"</h3></div>
-                    <h2 class='bounce-in-right'>WebDesign &nbsp; <span id="blue_text">Dev'Web</span></h2>
+                    <div class="main_text--metier"><h2 class='bounce-in-right'>WebDesign &nbsp; <span id="blue_text">Dev'Web</span></h2></div>
                         <div class="main_text--button">
                         Portfolio </div>
                 </div>
