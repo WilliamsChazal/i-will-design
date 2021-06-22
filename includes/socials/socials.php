b1963f75ba589599bf1432fd7874f7448e5cfff8
williams <w.chazal@codeur.online>
2021-06-22 16:34:36 +0200

    socials links

---
+++ includes/socials/socials.php
@@ -0,0 +1,10 @@
+
+
+<div class="social">
+<a href="https://github.com/WilliamsChazal">Github<i class="fab fa-github"></i></a>
+<a href="https://www.linkedin.com/in/williams-chazal-0a05a313a/">Linkedin<i class="fab fa-linkedin"></i></a>
+<a href="https://www.instagram.com/i_a_m_wills/">instagram<i class="fab fa-instagram"></i></a>
+
+
+</div>
+
