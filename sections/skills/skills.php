7052fe153c60401939daf001cbf4347b322ced1d
williams <w.chazal@codeur.online>
2021-06-28 16:32:34 +0200

    skills section

--- sections/skills/skills.php
+++ sections/skills/skills.php
@@ -1,8 +1,21 @@
 <div class='skills' id='skills_ancre'>
     <div class="skills_titre"><h2>Que sais je faire</h2></div>
+
     <div class="container_skills">
-        <div class="container_skills--conception"><h3>Conception</h3></div>
-        <div class="container_skills--realisation"><h3>Réalisation</h3></div>
-        <div class="container_skills--communication"><h3>Communication</h3></div>
+        <div class="container_skills--conception"><span><h3>Conception</h3></span>
+        <span><img src="" alt=""></span>
+        <span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
     </div>
+    <div class="container_skills">
+        <div class="container_skills--realisation"><span><h3>Réalisation</h3></span>
+        <span><img src="" alt=""></span>
+        <span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
+    </div>
+    <div class="container_skills">
+        <div class="container_skills--communication"><span><h3>Communication</h3></span>
+        <span><img src="" alt=""></span>
+        <span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
+    </div>
+    
+    
 </div>
