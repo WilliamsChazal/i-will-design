b40dc817233234524222fc25de69a38d7231f864
WilliamsChazal <w.chazal@codeur.online>
2021-06-28 18:55:44 +0200

    mise en forme section skills

--- sections/skills/skills.php
+++ sections/skills/skills.php
@@ -2,19 +2,19 @@
     <div class="skills_titre"><h2>Que sais je faire</h2></div>
 
     <div class="container_skills">
-        <div class="container_skills--conception"><span><h3>Conception</h3></span>
-        <span><img src="assets/images/outils-de-conception.png" alt=""  class='skills_image--size'></span>
-        <span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
+        <div><span class="container_skills--titre"><h3>Conception</h3></span>
+        <span class="container_skills--image"><img src="assets/images/outils-de-conception.png" alt=""  class='skills_image--size'></span>
+        <span class="container_skills--texte"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
     </div>
     <div class="container_skills">
-        <div class="container_skills--realisation"><span><h3>Réalisation</h3></span>
-        <span><img src="assets/images/codage-web.png" alt=""  class='skills_image--size'></span>
-        <span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
+        <div><span class="container_skills--titre"><h3>Réalisation</h3></span>
+        <span class="container_skills--image"><img src="assets/images/codage-web.png" alt=""  class='skills_image--size'></span>
+        <span class="container_skills--texte"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
     </div>
     <div class="container_skills">
-        <div class="container_skills--communication"><span><h3>Communication</h3></span>
-        <span><img src="assets/images/la-communication.png" alt="" class='skills_image--size'></span>
-        <span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
+        <div><span class="container_skills--titre"><h3>Communication</h3></span>
+        <span class="container_skills--image"><img src="assets/images/la-communication.png" alt="" class='skills_image--size'></span>
+        <span class="container_skills--texte"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nam ut facilis doloribus laboriosam maxime optio! Facere, dolor est deserunt ad maiores pariatur delectus. Nesciunt iusto atque consequatur perferendis aliquid!</p></span>
     </div>
     
     
