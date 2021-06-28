b29943805e333d1bf2b1e747ad7ea852a2dd62a0
WilliamsChazal <w.chazal@codeur.online>
2021-06-28 19:28:14 +0200

    travail section about-me

--- sections/about-me/about-me.php
+++ sections/about-me/about-me.php
@@ -1,11 +1,22 @@
-<div class="about-me_information">
-                    <h2>Qui suis je ?</h2>
-                    <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
-                    Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p>
+<div class="container">
+  <div class="texte"></div>
+  <div class="portrait"></div>
+</div>
+
+
+
+<!-- <div class="about-me_information">
+                    <div class="about-me_information--titre"><h2>Qui suis je ?</h2></div>
+
+                    <div class="about-me_information--texte">
+                        <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
+                        Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p></div>
+
                         <div class="about-me_information--button">
                             <div class="about-me_button">Curriculum Vitae</div> 
-                        </div>    
-                </div>
-                <div class="about-me_photo">
-                    <img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait">
+                        </div> 
+
 </div>
+<div class="about-me_photo">
+                    <img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait"></div>
+</div> -->
