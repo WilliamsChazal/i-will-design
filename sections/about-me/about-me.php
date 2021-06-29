970967f109974c704075ee8b6a1888add0d93999
williams <w.chazal@codeur.online>
2021-06-29 14:48:42 +0200

    responsive +/- ok

--- sections/about-me/about-me.php
+++ sections/about-me/about-me.php
@@ -1,22 +1,12 @@
-<!-- <div class="container">
-  <div class="texte"></div>
-  <div class="portrait"></div>
+<div class='about'>
+    <div class="about_titre"><h2>Que sais je faire</h2></div>
+<div class="container_info">
+    <div class="texte_info">
+      <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
+      Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p></div>
+    <div class="image_info"><img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait_info"></div>
+ 
+  
 </div>
- -->
 
 
-<div class="about-me_information">
-                    <div class="about-me_information--titre"><h2>Qui suis je ?</h2></div>
-
-                    <div class="about-me_information--texte">
-                        <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
-                        Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p></div>
-
-                        <div class="about-me_information--button">
-                            <div class="about-me_button">Curriculum Vitae</div> 
-                        </div> 
-
-</div>
-<div class="about-me_photo">
-                    <img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait"></div>
-</div>
